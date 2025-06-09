4fbecc3625572d5576c8b9fedb5c09d2a8ba64c4
Lorenzo Pattaro <lorenzopattaro@live.it>
2025-06-09 14:55:28 +0200

    aggiunta implementazione della ricerca full-text, User-story 4 completata

--- src/main/java/it/aulab/progetto_finale_java/repositories/ArticleRepository.java
+++ src/main/java/it/aulab/progetto_finale_java/repositories/ArticleRepository.java
@@ -2,7 +2,9 @@ package it.aulab.progetto_finale_java.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.aulab.progetto_finale_java.models.Article;
 import it.aulab.progetto_finale_java.models.Category;
@@ -16,4 +18,12 @@ public interface ArticleRepository extends ListCrudRepository<Article, Long> {
     List<Article> findByIsAcceptedTrue();
     List<Article> findByIsAcceptedFalse();
     List<Article> findByIsAcceptedNull();
+
+    @Query("SELECT a FROM Article a WHERE " +
+       "LOWER(a.title) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+       "LOWER(a.subtitle) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+       "LOWER(a.user.username) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR " +
+       "LOWER(a.category.name) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    List<Article> search(@Param("searchTerm") String searchTerm);
+
 }
